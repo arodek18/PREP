e0ddf236f71c4fa7cc50bfc0d9e075d8fc0ebe8d
Mukesh kumar <mukesh.kumar@arodek.com>
2019-09-28 15:22:31 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,200 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow
+//  */
+
+// import React, {Fragment} from 'react';
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Text,
+//   StatusBar,
+// } from 'react-native';
+
+// import {
+//   Header,
+//   LearnMoreLinks,
+//   Colors,
+//   DebugInstructions,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// const App = () => {
+//   return (
+//     <Fragment>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+//           <Header />
+//           {global.HermesInternal == null ? null : (
+//             <View style={styles.engine}>
+//               <Text style={styles.footer}>Engine: Hermes</Text>
+//             </View>
+//           )}
+//           <View style={styles.body}>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Step One</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
+//                 screen and then come back to see your edits.
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>See Your Changes</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <ReloadInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Debug</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <DebugInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Learn More</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Read the docs to discover what to do next:
+//               </Text>
+//             </View>
+//             <LearnMoreLinks />
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </Fragment>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
+
+// export default App;
+
+
+
+import React, {Component, Fragment} from 'react';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+
+import { Fonts } from './src/utils/Fonts';
+import Home from './Home';
+import Login from './Login';
+import Profile from './Profile';
+import Icons from './Icons';
+import Allmessage from './Allmessage';
+import Sendmessage from './Sendmessage';
+import Replymessage from './Replymessage';
+import Filescreen from './Filescreen';
+import Emergency from './Emergency';
+
+import Charts from './Charts';
+import Cardpage from './Cardpage';
+import { createSwitchNavigator,createAppContainer,createDrawerNavigator,createBottomTabNavigator,createStackNavigator } from 'react-navigation';
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+global.api_url = 'http://13.232.30.205/merpapi/api.php';
+
+const RootStack = createSwitchNavigator(
+  {
+    Home: { screen: Home },
+    Login: { screen: Login },
+    Profile : { screen: Profile },
+    Icons : { screen: Icons} ,
+    Allmessage : { screen : Allmessage },
+    Emergency : { screen : props => <Emergency {...props} defaultValue={'emergency'} /> },
+    ActiveShooter : { screen : props => <Emergency {...props} defaultValue={'activeShooter'} /> },
+    Sendmessage : { screen : Sendmessage },
+    Replymessage : { screen : Replymessage },
+    Filescreen : { screen : Filescreen },
+    Charts : { screen : Charts },
+    Cardpage : { screen : Cardpage },
+  },
+  {
+    initialRouteName: 'Login'
+  }
+
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Fragment>
+            <AppContainer/>
+        </Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontFamily: Fonts.Arimo
+    
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
